52316c6d858c8009c73763ec3b80b8e65ae8b80c
Fabian Schiller <schiller.fabian25@gmail.com>
2025-01-27 13:39:46 +0100

    [TASK] Move page.tsconfig to own file

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,29 +5,25 @@ defined('TYPO3') or die();
 
 // encapsulate all locally defined variables
 (static function() {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:cbgooglemaps/Configuration/TsConfig/ContentElementWizard.tsconfig">'
-    );
-    
     $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    
+
     $iconRegistry->registerIcon(
         'ce-default-icon',
         \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
         ['source' => 'EXT:cbgooglemaps/Resources/Public/Icons/ce_wiz.svg']
     );
-    
+
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
         'cbgooglemaps',
         'Quickgooglemap',
         [
             \Brinkert\Cbgooglemaps\Controller\MapController::class => 'index',
         ],
-    
+
         // non-cacheable actions
         []
     );
-    
+
     $tStamp = (new Datetime("now"))->getTimestamp();
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][$tStamp] = [
         'nodeName' => 'previewButtonElement',
