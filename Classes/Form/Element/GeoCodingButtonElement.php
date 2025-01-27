6647eb2714e330b65503f2990632438ab586070a
Fabian Schiller <schiller.fabian25@gmail.com>
2025-01-27 13:39:07 +0100

    [TASK] Update other classes to prevent PHP 8 warnings

--- Classes/Form/Element/GeoCodingButtonElement.php
+++ Classes/Form/Element/GeoCodingButtonElement.php
@@ -10,31 +10,35 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 class GeoCodingButtonElement extends AbstractFormElement
 {
 
-    public function render()
+    public function render(): array
     {
         /** @var ConfigurationManager $cm */
         $cm = GeneralUtility::makeInstance(ConfigurationManager::class);
         $ts = $cm->getConfiguration($cm::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
-        $settings = $ts['plugin.']['tx_cbgooglemaps.']['settings.'];
-        $i18n = $ts['plugin.']['tx_cbgooglemaps.']['_LOCAL_LANG.'];
-        $iso2 = $GLOBALS['BE_USER']->uc['lang'] . '.';
-        $btnLabels = isset($i18n[$iso2]) ? $i18n[$iso2] : $i18n['default.'];
+        if(isset($ts['plugin.']['tx_cbgooglemaps.'])) {
+            $settings = $ts['plugin.']['tx_cbgooglemaps.']['settings.'];
+            $i18n = $ts['plugin.']['tx_cbgooglemaps.']['_LOCAL_LANG.'];
+            $iso2 = $GLOBALS['BE_USER']->uc['lang'] . '.';
+            $btnLabels = isset($i18n[$iso2]) ? $i18n[$iso2] : $i18n['default.'];
 
-        $fieldset = '<div class="cbgm_geocoding">';
-        $fieldset .= '<input type="button" onclick="cbGooglemaps.doGeocoding(\'' . $this->data['databaseRow']['uid'] . '\',\''
-            . $settings['mapProvider'] . '\',\''
-            . $settings['mapboxapi.']['accessToken'] . '\')" value="'
-            . $btnLabels['btnGeocoding'] . '">';
-        $fieldset .= '<input type="button" onclick="cbGooglemaps.displayLocation(\'' . $this->data['databaseRow']['uid'] . '\',\''
-            . $settings['mapProvider'] . '\',\''
-            . $settings['mapboxapi.']['accessToken'] . '\')" value="'
-            . $btnLabels['btnDisplayMap'] . '">';
-        $fieldset .= '<div id="cbgm_previewLocation"></div>';
-        $fieldset .= '</div>';
+            $fieldset = '<div class="cbgm_geocoding">';
+            $fieldset .= '<input type="button" onclick="cbGooglemaps.doGeocoding(\'' . $this->data['databaseRow']['uid'] . '\',\''
+                . $settings['mapProvider'] . '\',\''
+                . $settings['mapboxapi.']['accessToken'] . '\')" value="'
+                . $btnLabels['btnGeocoding'] . '">';
+            $fieldset .= '<input type="button" onclick="cbGooglemaps.displayLocation(\'' . $this->data['databaseRow']['uid'] . '\',\''
+                . $settings['mapProvider'] . '\',\''
+                . $settings['mapboxapi.']['accessToken'] . '\')" value="'
+                . $btnLabels['btnDisplayMap'] . '">';
+            $fieldset .= '<div id="cbgm_previewLocation"></div>';
+            $fieldset .= '</div>';
 
 
-        $result = $this->initializeResultArray();
-        $result['html'] = $fieldset;
-        return $result;
+            $result = $this->initializeResultArray();
+            $result['html'] = $fieldset;
+            return $result;
+        } else {
+            return [];
+        }
     }
 }
--- Classes/Form/Element/PreviewButtonElement.php
+++ Classes/Form/Element/PreviewButtonElement.php
@@ -9,31 +9,34 @@ use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
 class PreviewButtonElement extends AbstractFormElement
 {
 
-    public function render()
+    public function render(): array
     {
 
         /** @var ConfigurationManager $cm */
         $cm = GeneralUtility::makeInstance(ConfigurationManager::class);
         $ts = $cm->getConfiguration($cm::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
-        $settings = $ts['plugin.']['tx_cbgooglemaps.']['settings.'];
-        $i18n = $ts['plugin.']['tx_cbgooglemaps.']['_LOCAL_LANG.'];
-        $iso2 = $GLOBALS['BE_USER']->uc['lang'] . '.';
-        $btnLabels = isset($i18n[$iso2]) ? $i18n[$iso2] : $i18n['default.'];
+            if(isset($ts['plugin.']['tx_cbgooglemaps.'])) {
+                $settings = $ts['plugin.']['tx_cbgooglemaps.']['settings.'];
+                $i18n = $ts['plugin.']['tx_cbgooglemaps.']['_LOCAL_LANG.'];
+                $iso2 = $GLOBALS['BE_USER']->uc['lang'] . '.';
+                $btnLabels = isset($i18n[$iso2]) ? $i18n[$iso2] : $i18n['default.'];
+                $fieldset =  '<div class="cbgm_preview">';
+                $fieldset .= '<input type="button" onclick="cbGooglemaps.displayPreview(\''
+                    . $this->data['vanillaUid'].'\',\''
+                    . $settings['display.']['zoom'].'\',\''
+                    . $settings['display.']['mapType'].'\',\''
+                    . $settings['display.']['navigationControl'].'\',\''
+                    . $settings['mapProvider'] .'\',\''
+                    . $settings['mapboxapi.']['accessToken'] .'\')" value="'
+                    . $btnLabels['btnPreviewMap'] .'">';
+                $fieldset .= '<div id="cbgm_previewMap"></div>';
+                $fieldset .= '</div>';
 
-        $fieldset =  '<div class="cbgm_preview">';
-        $fieldset .= '<input type="button" onclick="cbGooglemaps.displayPreview(\''
-            . $this->data['vanillaUid'].'\',\''
-            . $settings['display.']['zoom'].'\',\''
-            . $settings['display.']['mapType'].'\',\''
-            . $settings['display.']['navigationControl'].'\',\''
-            . $settings['mapProvider'] .'\',\''
-            . $settings['mapboxapi.']['accessToken'] .'\')" value="'
-            . $btnLabels['btnPreviewMap'] .'">';
-        $fieldset .= '<div id="cbgm_previewMap"></div>';
-        $fieldset .= '</div>';
-
-        $result = $this->initializeResultArray();
-        $result['html'] = $fieldset;
-        return $result;
+            $result = $this->initializeResultArray();
+            $result['html'] = $fieldset;
+            return $result;
+        } else {
+            return [];
+        }
     }
 }
